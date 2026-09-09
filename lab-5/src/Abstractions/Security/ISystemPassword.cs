d2dfad55389a61ec41e17d941a25e68aa0a34d63
MarinaBalakina <marinabalakina132@gmail.com>
2026-09-09 22:48:58 +0300

    Add files via upload

---
+++ lab-5/src/Abstractions/Security/ISystemPassword.cs
@@ -0,0 +1,6 @@
+namespace Itmo.ObjectOrientedProgramming.Lab5.Abstractions.Security;
+
+public interface ISystemPassword
+{
+    string GetPassword();
+}
---
+++ lab-5/src/Abstractions/Time/IClock.cs
@@ -0,0 +1,6 @@
+namespace Itmo.ObjectOrientedProgramming.Lab5.Abstractions.Time;
+
+public interface IClock
+{
+    DateTimeOffset Now();
+}
